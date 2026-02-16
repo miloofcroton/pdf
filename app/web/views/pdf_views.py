d46241b70f966c809ac3a942b3563ad8decb5bc1
Jack Toumey <jack@toumey.io>
2026-02-16 16:34:54 -0700

    formatting and delay processing

--- app/web/views/pdf_views.py
+++ app/web/views/pdf_views.py
@@ -11,34 +11,33 @@ bp = Blueprint("pdf", __name__, url_prefix="/api/pdfs")
 @bp.route("/", methods=["GET"])
 @login_required
 def list():
-    pdfs = Pdf.where(user_id=g.user.id)
+  pdfs = Pdf.where(user_id=g.user.id)
 
-    return Pdf.as_dicts(pdfs)
+  return Pdf.as_dicts(pdfs)
 
 
 @bp.route("/", methods=["POST"])
 @login_required
 @handle_file_upload
 def upload_file(file_id, file_path, file_name):
-    res, status_code = files.upload(file_path)
-    if status_code >= 400:
-        return res, status_code
+  res, status_code = files.upload(file_path)
+  if status_code >= 400:
+    return res, status_code
 
-    pdf = Pdf.create(id=file_id, name=file_name, user_id=g.user.id)
+  pdf = Pdf.create(id=file_id, name=file_name, user_id=g.user.id)
 
-    # TODO: Defer this to be processed by the worker
-    process_document(pdf.id)
+  process_document.delay(pdf.id)
 
-    return pdf.as_dict()
+  return pdf.as_dict()
 
 
 @bp.route("/<string:pdf_id>", methods=["GET"])
 @login_required
 @load_model(Pdf)
 def show(pdf):
-    return jsonify(
-        {
-            "pdf": pdf.as_dict(),
-            "download_url": files.create_download_url(pdf.id),
-        }
-    )
+  return jsonify(
+    {
+      "pdf": pdf.as_dict(),
+      "download_url": files.create_download_url(pdf.id),
+    }
+  )
