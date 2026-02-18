21bc98af63c7892297001dd09e75372a371bda31
Jack Toumey <jack@toumey.io>
2026-02-18 11:15:16 -0700

    isolating the queue

--- test.py
+++ test.py
@@ -6,29 +6,32 @@ from queue import Queue
 from threading import Thread
 
 
-queue = Queue()
-
-
 class StreamingHandler(BaseCallbackHandler):
+  def __init__(self, queue):
+    self.queue = queue
+
   def on_llm_new_token(self, token, **kwargs):
-    queue.put(token)
+    self.queue.put(token)
 
   def on_llm_end(self, response, **kwargs):
-    queue.put(None)
+    self.queue.put(None)
 
   def on_llm_error(self, error, **kwargs):
-    queue.put(None)
+    self.queue.put(None)
 
 
-chat = ChatOpenAI(streaming=True, callbacks=[StreamingHandler()])
+chat = ChatOpenAI(streaming=True)
 
 prompt = ChatPromptTemplate.from_messages([("human", "{content}")])
 
 
 class StreamingChain(LLMChain):
   def stream(self, input):
+    queue = Queue()
+    handler = StreamingHandler(queue)
+
     def task():
-      self(input)
+      self(input, callbacks=[handler])
 
     Thread(target=task).start()
     while True:
