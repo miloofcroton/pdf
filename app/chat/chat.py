b107c83670bca917b2b898168369d5db63b00b25
Jack Toumey <jack@toumey.io>
2026-02-19 10:58:29 -0700

    adding scoring

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -5,7 +5,11 @@ from app.chat.vector_stores import retriever_map
 from app.chat.llms import llm_map
 from app.chat.memories import memory_map
 from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
-from app.web.api import set_conversation_components, get_conversation_components
+from app.web.api import (
+  set_conversation_components,
+  get_conversation_components,
+)
+from app.chat.score import random_component_by_score
 
 
 def select_component(component_type, component_map, chat_args):
@@ -16,7 +20,7 @@ def select_component(component_type, component_map, chat_args):
     builder = component_map[previous_component]
     return previous_component, builder(chat_args)
   else:
-    random_name = random.choice(list(component_map.keys()))
+    random_name = random_component_by_score(component_type, component_map)
     builder = component_map[random_name]
     return random_name, builder(chat_args)
 
