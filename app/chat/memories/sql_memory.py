88c99acb9d1bc074b12bef00b7db1cfb24ecc11f
Jack Toumey <jack@toumey.io>
2026-02-19 10:19:24 -0700

    making chat components more modular

--- app/chat/memories/sql_memory.py
+++ app/chat/memories/sql_memory.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel  # pylint: disable=import-error
+from pydantic import BaseModel
 from langchain.memory import ConversationBufferMemory
 from langchain_core.chat_history import BaseChatMessageHistory
 
@@ -28,9 +28,7 @@ class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
 
 def build_memory(chat_args):
   return ConversationBufferMemory(
-    chat_memory=SqlMessageHistory(
-      conversation_id=chat_args.conversation_id,
-    ),
+    chat_memory=SqlMessageHistory(conversation_id=chat_args.conversation_id),
     return_messages=True,
     memory_key="chat_history",
     output_key="answer",
